740b06cbc57fbe20c0d5374a6118ab69840c58cb
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 08:44:39 +0200

    Added xml comments to constants

--- BitConstants.cs
+++ BitConstants.cs
@@ -2,17 +2,40 @@ namespace BitTools;
 
 public static class BitConstants
 {
-    // INTEGRAL TYPES
+    /// <summary>
+    /// The amount of bits in a value of type byte (and sbyte)
+    /// </summary>
     public const int BitsInByte = 8;
+    /// <summary>
+    /// The amount of bits in a value of type short (and ushort)
+    /// </summary>
     public const int BitsInShort = 16;
+    /// <summary>
+    /// The amount of bits in a value of type int (and uint)
+    /// </summary>
     public const int BitsInInt = 32;
+    /// <summary>
+    /// The amount of bits in a value of type long (and ulong)
+    /// </summary>
     public const int BitsInLong = 64;
 
-    // FLOATING POINT TYPES
+
+    /// <summary>
+    /// The amount of bits in a value of type float
+    /// </summary>
     public const int BitsInFloat = 32;
+    /// <summary>
+    /// The amount of bits in a value of type double
+    /// </summary>
     public const int BitsInDouble = 64;
+    /// <summary>
+    /// The amount of bits in a value of type decimal
+    /// </summary>
     public const int BitsInDecimal = 128;
 
-    // CHARACTER TYPE
+
+    /// <summary>
+    /// The amount of bits in a value of type char
+    /// </summary>
     public const int BitsInChar = 16;
 }
