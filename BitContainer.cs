65dbcb65b7a205ad49813a5fabba42f95c9c8beb
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 11:22:07 +0200

    Added xml documentation to BitContainer class

--- BitContainer.cs
+++ BitContainer.cs
@@ -1,20 +1,56 @@
 namespace BitTools;
 
+/// <summary>
+/// Methods for changing the container of some bits, without changing the bits themselves
+/// </summary>
 public static class BitContainer
 {
-    public static byte SByteToByte(sbyte val) => unchecked((byte)val);
-
-    public static sbyte ByteToSByte(byte val) => unchecked((sbyte)val);
-
-    public static short UShortToShort(ushort val) => unchecked((short)val);
-
-    public static ushort ShortToUShort(short val) => unchecked((ushort)val);
-
-    public static int UIntToInt(uint val) => unchecked((int)val);
-
-    public static uint IntToUInt(int val) => unchecked((uint)val);
-
-    public static long ULongToLong(ulong val) => unchecked((long)val);
-
-    public static ulong LongToULong(long val) => unchecked((ulong)val);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="byte" /> with the exact same bits as <paramref name="value"/></returns>
+    public static byte SByteToByte(sbyte value) => unchecked((byte)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="sbyte" /> with the exact same bits as <paramref name="value"/></returns>
+    public static sbyte ByteToSByte(byte value) => unchecked((sbyte)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="short" /> with the exact same bits as <paramref name="value"/></returns>
+    public static short UShortToShort(ushort value) => unchecked((short)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="ushort" /> with the exact same bits as <paramref name="value"/></returns>
+    public static ushort ShortToUShort(short value) => unchecked((ushort)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="int" /> with the exact same bits as <paramref name="value"/></returns>
+    public static int UIntToInt(uint value) => unchecked((int)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="uint" /> with the exact same bits as <paramref name="value"/></returns>
+    public static uint IntToUInt(int value) => unchecked((uint)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="long" /> with the exact same bits as <paramref name="value"/></returns>
+    public static long ULongToLong(ulong value) => unchecked((long)value);
+    /// <summary>
+    /// Changes the container of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The number</param>
+    /// <returns>A <see langword="ulong" /> with the exact same bits as <paramref name="value"/></returns>
+    public static ulong LongToULong(long value) => unchecked((ulong)value);
 }
