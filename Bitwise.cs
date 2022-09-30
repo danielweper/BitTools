50effa19e6d84239bfeca24d9824121b64d2c0f3
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 09:46:39 +0200

    Added nor function

--- Bitwise.cs
+++ Bitwise.cs
@@ -22,14 +22,14 @@ public static class Bitwise
     public static ulong Or(ulong value1, ulong value2) => value1 | value2;
 
 
-    public static byte And(byte value1, byte value2) => (byte)(value1 & value2);
-    public static sbyte And(sbyte value1, sbyte value2) => (sbyte)(value1 & value2);
-    public static short And(short value1, short value2) => (short)(value1 & value2);
-    public static ushort And(ushort value1, ushort value2) => (ushort)(value1 & value2);
-    public static int And(int value1, int value2) => value1 & value2;
-    public static uint And(uint value1, uint value2) => value1 & value2;
-    public static long And(long value1, long value2) => value1 & value2;
-    public static ulong And(ulong value1, ulong value2) => value1 & value2;
+    public static byte Nor(byte value1, byte value2) => Complement(Or(value1, value2));
+    public static sbyte Nor(sbyte value1, sbyte value2) => Complement(Or(value1, value2));
+	public static short Nor(short value1, short value2) => Complement(Or(value1, value2));
+	public static ushort Nor(ushort value1, ushort value2) => Complement(Or(value1, value2));
+	public static int Nor(int value1, int value2) => Complement(Or(value1, value2));
+	public static uint Nor(uint value1, uint value2) => Complement(Or(value1, value2));
+	public static long Nor(long value1, long value2) => Complement(Or(value1, value2));
+	public static ulong Nor(ulong value1, ulong value2) => Complement(Or(value1, value2));
 
 
     public static byte Xor(byte value1, byte value2) => And(Or(value1, value2), Complement(And(value1, value2)));
@@ -40,4 +40,15 @@ public static class Bitwise
 	public static uint Xor(uint value1, uint value2) => And(Or(value1, value2), Complement(And(value1, value2)));
 	public static long Xor(long value1, long value2) => And(Or(value1, value2), Complement(And(value1, value2)));
 	public static ulong Xor(ulong value1, ulong value2) => And(Or(value1, value2), Complement(And(value1, value2)));
+
+
+
+    public static byte And(byte value1, byte value2) => (byte)(value1 & value2);
+    public static sbyte And(sbyte value1, sbyte value2) => (sbyte)(value1 & value2);
+    public static short And(short value1, short value2) => (short)(value1 & value2);
+    public static ushort And(ushort value1, ushort value2) => (ushort)(value1 & value2);
+    public static int And(int value1, int value2) => value1 & value2;
+    public static uint And(uint value1, uint value2) => value1 & value2;
+    public static long And(long value1, long value2) => value1 & value2;
+    public static ulong And(ulong value1, ulong value2) => value1 & value2;
 }
