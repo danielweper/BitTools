28a58162dafa53fecd56e451a0f2e158d78240f6
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 09:16:56 +0200

    Start of Bitwise class, with OR implemented for all integral types

---
+++ Bitwise.cs
@@ -0,0 +1,13 @@
+namespace BitTools;
+
+public static class Bitwise
+{
+    public static byte Or(byte value1, byte value2) => (byte)(value1 | value2);
+    public static sbyte Or(sbyte value1, sbyte value2) => (sbyte)(value1 | value2);
+    public static short Or(short value1, short value2) => (short)(value1 | value2);
+    public static ushort Or(ushort value1, ushort value2) => (ushort)(value1 | value2);
+    public static int Or(int value1, int value2) => value1 | value2;
+    public static uint Or(uint value1, uint value2) => value1 | value2;
+    public static long Or(long value1, long value2) => value1 | value2;
+    public static ulong Or(ulong value1, ulong value2) => value1 | value2;
+}
