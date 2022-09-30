ecd3b6fda6aece1dfeb7dac92e74d0f2beb91919
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 09:26:28 +0200

    Added Bitwise.And function for all integral types

--- Bitwise.cs
+++ Bitwise.cs
@@ -10,4 +10,14 @@ public static class Bitwise
     public static uint Or(uint value1, uint value2) => value1 | value2;
     public static long Or(long value1, long value2) => value1 | value2;
     public static ulong Or(ulong value1, ulong value2) => value1 | value2;
+
+
+    public static byte And(byte value1, byte value2) => (byte)(value1 & value2);
+    public static sbyte And(sbyte value1, sbyte value2) => (sbyte)(value1 & value2);
+    public static short And(short value1, short value2) => (short)(value1 & value2);
+    public static ushort And(ushort value1, ushort value2) => (ushort)(value1 & value2);
+    public static int And(int value1, int value2) => value1 & value2;
+    public static uint And(uint value1, uint value2) => value1 & value2;
+    public static long And(long value1, long value2) => value1 & value2;
+    public static ulong And(ulong value1, ulong value2) => value1 & value2;
 }
